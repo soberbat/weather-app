76d3842668f1cdfd54a8eb0ef9fd130362c5e3b7
Berat Genç <beratgenc470@gmail.com>
2022-04-15 23:47:04 +0300

    data fetching and dynamic routes

---
+++ pages/city/[id]/index.js
@@ -0,0 +1,18 @@
+// export async function getServerSideProps({ query }) {
+//   const { id } = query;
+
+//   const res = await fetch(
+//     `https://api.openweathermap.org/data/2.5/weather?q=${id}&appid=00b976e91b2b6e21b881af1759eb0249`
+//   );
+
+//   const data = await res.json();
+
+//   return {
+//     props: { data },
+//   };
+// }
+
+export default function City({ data }) {
+  console.log(data);
+  return <div>bok ye</div>;
+}
---
+++ styles/İndex.styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: #232323;
+  height: 100vh;
+
+  width: 100vw;
+`;
